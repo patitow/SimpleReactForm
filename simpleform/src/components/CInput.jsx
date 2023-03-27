aa5943763ef925548d46b86374e922d4bb9c4755
patitow <msoliver2002@gmail.com>
2023-03-27 20:52:26 -0300

    tailwind css + input customizado

--- simpleform/src/components/CInput.jsx
+++ simpleform/src/components/CInput.jsx
@@ -1,12 +1,24 @@
+import '../css/index.css'
 
-function CInput(){
+//componente input customizado que recebe propriedades de type e label.
+function CInput({label,type}) {
 
-
-    return(
+    const typesOfInput = ["button","checkbox","color","date","datetime","datetime-local","email","file","hidden","image","month","number","password","radio","range",
+    "reset","search","submit","tel","text","time","url","week"];
+    
+    return (
         <>
-        <input className="rounded-lg"></input>
+            <label className="text-lg font-normal leading-tight text-gray-800" for={label}>
+                {label.charAt(0).toUpperCase() + label.slice(1)}
+            </label>
+            <input className="border rounded-full w-full py-3 px-4 text-gray-700 leading-tight focus:outline-offset-0 focus:shadow-outline focus:outline-blue-900" 
+            id={label} 
+            type={typesOfInput.indexOf(type) === -1 ? "text" : type }
+            placeholder=". . ."></input>
         </>
     )
+
+    
 }
 
 export default CInput
--- simpleform/src/main.jsx
+++ simpleform/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './css/index.css'
 import CInput from './components/CInput'
+import Header from './components/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <CInput></CInput>
+    {/* <CInput label="teste" type="number"></CInput> */}
+    <Header></Header>
   </React.StrictMode>,
 )
