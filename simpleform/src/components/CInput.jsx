22ad4c3db4890133ca554dced1448658ace0eb0e
patitow <msoliver2002@gmail.com>
2023-03-28 14:25:01 -0300

    apagando codigo desnecessario em CInput

--- simpleform/src/components/CInput.jsx
+++ simpleform/src/components/CInput.jsx
@@ -5,8 +5,6 @@ function CInput({label,type}) {
 
     const typesOfInput = ["button","checkbox","color","date","datetime","datetime-local","email","file","hidden","image","month","number","password","radio","range",
     "reset","search","submit","tel","text","time","url","week"];
-    const defaultInputClassName="input mt-2 border rounded-full w-full py-3 px-4 text-gray-700 leading-tight focus:outline-offset-0 focus:shadow-outline focus:outline-blue-900 text-center";
-    const defaultLabelClassName="label text-2xl font-semibold leading-tight text-gray-800 text-center mt-3";    
 
     return (
         <>
